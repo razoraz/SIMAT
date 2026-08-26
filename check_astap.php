e50a91519a0b86143b29f2c2e1baae7d79f40677
razoraz <vramadhan851@gmail.com>
2026-08-26 08:35:42 +0700

    astaps

--- check_astap.php
+++ check_astap.php
@@ -3,9 +3,20 @@ require 'vendor/autoload.php';
 $app = require 'bootstrap/app.php';
 $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-$astaps = \App\Models\Astap::with('jenisAstap')->get();
-foreach ($astaps as $a) {
-    $jaNama = $a->jenisAstap ? $a->jenisAstap->uraian_sub_sub_rincian : 'NULL';
-    $jaKode = $a->jenisAstap ? $a->jenisAstap->sub_sub_rincian_objek : 'NULL';
-    echo "ID: {$a->id} | nama_barang: {$a->nama_barang} | kode_108: {$a->kode_108} | jaKode: {$jaKode} | jaNama: {$jaNama}" . PHP_EOL;
+$astaps = \App\Models\Astap::with(['registers', 'jenisAstap', 'rekeningBelanja', 'jenisPengadaan', 'unit'])
+    ->orderBy('id', 'desc')
+    ->get()
+    ->map(function($a) {
+        $spec = is_array($a->spesifikasi_json) ? $a->spesifikasi_json : (json_decode($a->spesifikasi_json, true) ?? []);
+        return [
+            'id' => $a->id,
+            'category' => $a->category,
+            'kode_barang' => $a->kode_108,
+            'nama_barang' => $a->nama_barang,
+        ];
+    });
+
+foreach ($astaps as $item) {
+    echo "ID: {$item['id']} | cat: {$item['category']} | kode: {$item['kode_barang']} | nama: {$item['nama_barang']}" . PHP_EOL;
 }
+echo "Total: " . $astaps->count() . PHP_EOL;
